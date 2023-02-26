7f100611777156e52d63563b63c22f66bd9bf124
Adrian <adrian@adrians-air.home>
2023-02-26 18:03:30 +0000

    Fig 26Feb

--- R/instructions.R
+++ R/instructions.R
@@ -3,6 +3,10 @@ info_page <- function(id) {
                               button_text = psychTestR::i18n("AMDI_0016_I_0001_1"))
 }
 
+info_image_page <- function(url) {
+  psychTestR::one_button_page(div(p(img(src = url, width="100%")), p("")))
+}
+
 audio_ex_page <- function(prompt_id, url) {
   psychTestR::audio_NAFC_page(
     label = "ex",
@@ -21,7 +25,14 @@ instructions <- function(media_dir, num_items) {
     psychTestR::while_loop(
       test = function(state, ...) psychTestR::get_local("do_intro", state),
       logic = c(
-        info_page("AMDI_0002_I_0001_1"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/1.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/2.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/3.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/4.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/5.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/6.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/7.jpg"),
+        info_image_page("https://raw.githubusercontent.com/chloekhc/mdt/master/fig/8.jpg"),
         audio_ex_page("AMDI_0003_I_0001_1", file.path(media_dir, "examples/ex1.mp3")),
         info_page("AMDI_0004_I_0001_1"),
         practice(media_dir),
