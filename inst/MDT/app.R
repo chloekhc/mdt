62d608187f9297c9063dddc77f93fc605a869891
Adrian <adrian@Adrians-Air.Home>
2023-03-11 13:34:14 +0000

    Test 2

--- inst/MDT/app.R
+++ inst/MDT/app.R
@@ -1,4 +1,7 @@
 remove.packages("mdt")
+remove.packages("psychTestR")
+
+devtools::install_local("/Users/adrian/psychTestR")
 
 devtools::install_local("/Users/adrian/Projects/mdt")
 
@@ -7,4 +10,4 @@ library(htmltools)
 
 standalone_mdt(num_items = 20, take_training = TRUE)
 # with training
-# standalone_mdt(num_items = 18, take_training = FALSE)
+ standalone_mdt(num_items = 18, take_training = FALSE)
